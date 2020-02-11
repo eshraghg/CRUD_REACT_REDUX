1fc199fa4e68a2407de32045e720e18973690486
Eshargh Ghoodjani <e.ghoodjani@gmail.com>
2020-02-11 21:14:58 +0330

    second commit

---
+++ src/components/GoogleAuth.js
@@ -0,0 +1,74 @@
+import React from "react";
+import { connect } from "react-redux";
+import { signIn, signOut } from "../actions";
+
+class GoogleAuth extends React.Component {
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client
+        .init({
+          clientId:
+            "1084997908212-hmbs3n9fmnti7hj26tdeod8uag11oue0.apps.googleusercontent.com",
+          scope: "email"
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.onAuthChange(this.auth.isSignedIn.get());
+          this.auth.isSignedIn.listen(this.onAuthChange);
+        });
+    });
+  }
+
+  onAuthChange = isSignedIn => {
+    if (isSignedIn) {
+      this.props.signIn(this.auth.currentUser.get().getId());
+    } else {
+      this.props.signOut();
+    }
+  };
+
+  onSignInClick = () => {
+    this.auth.signIn();
+  };
+
+  onSignOutClick = () => {
+    this.auth.signOut();
+  };
+
+  renderAuthButton() {
+    if (this.props.isSignedIn === null) {
+      return <div>Loading...</div>;
+    } else if (this.props.isSignedIn) {
+      return (
+        <div>
+          <button
+            onClick={this.onSignOutClick}
+            className="ui red google button"
+          >
+            <i className="google icon" />
+            Sign out
+          </button>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <button onClick={this.onSignInClick} className="ui red google button">
+            <i className="google icon" />
+            Sign in with Google
+          </button>
+        </div>
+      );
+    }
+  }
+
+  render() {
+    return <div> {this.renderAuthButton()} </div>;
+  }
+}
+
+const mapStateToProps = state => {
+  return { isSignedIn: state.auth.isSignedIn };
+};
+
+export default connect(mapStateToProps, { signIn, signOut })(GoogleAuth);
